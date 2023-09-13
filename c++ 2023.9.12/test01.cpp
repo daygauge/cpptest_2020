823a27dd2a67abefc452eb9a4bb5b08df19316fa
daygauge <hw6ii00e@163.com>
2023-09-13 19:31:32 +0800

    9.13

--- c++ 2023.9.12/test01.cpp
+++ c++ 2023.9.12/test01.cpp
@@ -2,8 +2,20 @@
 
 void test01()
 {
-    vector<int> arr = {3, 2, 1};
-    Solution s;
-    vector<int> v = s.getLeastNumbers(arr, 2);
-    for_each(v.begin(), v.end(), printfor());
+    // vector<int> arr = {3, 2, 1};
+    // Solution s;
+    // vector<int> v = s.getLeastNumbers(arr, 2);
+    // for_each(v.begin(), v.end(), printfor());
+    test02();
+}
+void test02()
+{
+    // vector<int> v1 = {110, 120, 130};
+    // vector<int> v2 = {210, 220, 230};
+    // auto f = [](int a, int b) -> int
+    // { return a + b + 10; };
+    // cout << f(1, 9) << endl;
+    // Solution913 S;
+    // vector<int> v = {10, 2};
+    // cout << S.minNumber(v) << endl;
 }
--- c++ 2023.9.12/test01.h
+++ c++ 2023.9.12/test01.h
@@ -59,7 +59,7 @@ public:
         cout << val << " ";
     }
 };
-
+void test02();
 void test01();
 
 // 剑指 Offer 41
@@ -122,5 +122,75 @@ public:
     double findMedian()
     {
         return a.size() == b.size() ? (a.top() + b.top()) / 2.0 : a.top();
+    }
+};
+
+// 剑指 Offer 45
+class Solution913
+{
+public:
+    string minNumber(vector<int> &nums)
+    {
+        vector<string> v;
+        for (int i = 0; i < nums.size(); i++)
+            v.push_back(to_string(nums[i]));
+        quickSort(v, 0, v.size() - 1);
+        string ls;
+        for (string s : v)
+            ls.append(s);
+        return ls;
+    }
+    void quickSort(vector<string> &v, int left, int right)
+    {
+        if (left >= right)
+            return;
+        int L = left, R = right;
+        while (L < R)
+        {
+            while (v[R] + v[left] >= v[left] + v[R] && L < R)
+                R--;
+            while (v[L] + v[left] <= v[left] + v[L] && L < R)
+                L++;
+
+            swap(v[L], v[R]);
+        }
+        swap(v[left], v[L]);
+        quickSort(v, left, L - 1);
+        quickSort(v, L + 1, right);
+    }
+
+    void quickSort1(vector<string> &strs, int l, int r)
+    {
+        if (l >= r)
+            return;
+        int i = l, j = r;
+        while (i < j)
+        {
+            while (strs[j] + strs[l] >= strs[l] + strs[j] && i < j)
+                j--;
+            while (strs[i] + strs[l] <= strs[l] + strs[i] && i < j)
+                i++;
+            swap(strs[i], strs[j]);
+        }
+        swap(strs[i], strs[l]);
+        quickSort(strs, l, i - 1);
+        quickSort(strs, i + 1, r);
+    }
+};
+
+// 剑指 Offer 61
+class Solution9132
+{
+public:
+    bool isStraight(vector<int> &nums)
+    {
+        sort(nums.begin(), nums.end(), less<int>());
+        int i = 0;
+        while (nums[i] == 0)
+            i++;
+        for (int s = i; s < nums.size() - 1; s++)
+            if (nums[s] == nums[s + 1])
+                return false;
+        return nums[nums.size() - 1] - nums[i] < 5 ? true : false;
     }
 };
